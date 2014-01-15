12e1c7d8682a280a26ff7a515d558cf6eb3aee1c
Hashini Senaratne <hashz1990@gmail.com>
2014-01-15 17:28:19 +0530

    Basic service and user

---
+++ MathUser/MathUser/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathUser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            localhost.MathService myMathService = new localhost.MathService();
+            Console.Write("2 + 4 = {0}", myMathService.Add(2, 4));
+            Console.ReadLine();
+        }
+    }
+}
